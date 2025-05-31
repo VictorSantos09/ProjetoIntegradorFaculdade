1716669b6e0b850fb8f183c8663c61d39ffe15dd
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2025-05-31 19:06:32 -0300

    Melhora configuração

---
+++ PrevencaoIncendio/Caching/CacheConnectionString.cs
@@ -0,0 +1,12 @@
+﻿namespace PrevencaoIncendio.Caching;
+
+public class CacheConnectionString
+{
+    public string EndPoints { get; set; }
+    public string Password { get; set; }
+    public bool Ssl { get; set; }
+    public string SslHost { get; set; }
+    public string User { get; set; }
+    public bool AbortOnConnectFail { get; set; }
+}
+
